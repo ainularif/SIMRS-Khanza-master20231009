ddbe0849b9854446d6eceab060ce6863df6bc25b
Fika <fikalovita08@gmail.com>
2024-05-29 09:23:27 +0700

    ganti ygl update

--- src/simrskhanza/frmUtama.java
+++ src/simrskhanza/frmUtama.java
@@ -7278,7 +7278,7 @@ public class frmUtama extends javax.swing.JFrame {
         jLabel7.setForeground(new java.awt.Color(50, 50, 50));
         jLabel7.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/file-edit-16x16.png"))); // NOI18N
-        jLabel7.setText(" Didesain & dibuat oleh Khanza.Soft Media - Update 2023/10/16 - 20240524");
+        jLabel7.setText(" Didesain & dibuat oleh Khanza.Soft Media - Update 2023/10/16 - 20240529");
         jLabel7.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
         jLabel7.setIconTextGap(3);
         jLabel7.setName("jLabel7"); // NOI18N
